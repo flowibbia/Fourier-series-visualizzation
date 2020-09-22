95dbf0f7128044bdde1db5988af751fb43a14b4a
flowibbia <flowing@mail.com>
2020-09-22 10:35:44 +0200

    sketch predisposto a lavorare con gli array

--- public/sketch.js
+++ public/sketch.js
@@ -1,9 +1,26 @@
 let time  =0;
 
-//let coefficients= require('./array');
+let coefficients = [];
 
-// console.log(coefficients[0]);
-// console.log(coefficients[1]);
+class coefficient {
+  constructor(_radius, _angle) {
+    this.radius = _radius;
+    this.angle = _angle;
+  }
+}
+
+
+//get coefficient automatically for rectangular impulse
+for(let i=0;i<40;i++){
+
+    let n= i*2 +1;
+    let  radius = 50 * (4 / (n*3.14));
+    let coef= new coefficient(radius,n);
+
+    coefficients.push(coef);
+    
+  console.log(coefficients[i].radius);
+}
 
 let wave = [];  //point of the function
 let num=10;     //number of coefficient
@@ -12,36 +29,33 @@ let num=10;     //number of coefficient
 function setup(){
 
   let cnv=createCanvas(1000,400);
-    //cnv.position(0,0);
-  //createCanvas(600, 400);
 }
 
 function draw(){
 
-  background(0);
+  background(0,10,50);
 
   translate(200,200);
 
   let x=0;
   let y=0;
 
-
   for(let i=0;i<num;i++){
 
-    let prevx=x;
-    let prevy=y;
+     let prevx=x;
+     let prevy=y;
 
-    let n= i*2 +1;
+    // let n= i*2 +1;
 
-    let  radius = 50 * (4 / (n*PI));
+    // let  radius = 50 * (4 / (n*PI));
 
-    x += radius *  cos(n*time);
-    y += radius *  sin(n*time);
+    x += coefficients[i].radius *  cos((i*2+1)*time);  //todo aggiustare l'indice di sfasamento
+    y += coefficients[i].radius *  sin((i*2+1)*time);
 
     noFill();
     stroke(120);
 
-    ellipse(prevx,prevy,radius*2);
+    ellipse(prevx,prevy,coefficients[i].radius*2);
 
     if(i==num-1)
       wave.unshift(y);
@@ -67,6 +81,6 @@ function draw(){
   }
 
   endShape();
-  time+=0.005;
+  time+=0.05;
 
 }
