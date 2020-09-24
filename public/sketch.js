72de24bc46595401b2ae10d3676cbfd6f39a0575
flowibbia <flowing@mail.com>
2020-09-24 10:30:40 +0200

    messi coefficienti rect nel server

--- public/sketch.js
+++ public/sketch.js
@@ -1,10 +1,64 @@
+const xhr = new XMLHttpRequest();
+
+xhr.onload= ()=>{
+
+}
+
 let time  =0;
 let coefficients0 = [];  //array for debug
 
+class coefficient {
+  constructor(_radius, _angle) {
+    this.radius = _radius;
+    this.angle = _angle;
+  }
+}
+
+//get coefficient automatically for rectangular impulse and store in the array for debug 
+for(let i=0;i<40;i++){
+
+    let n= i*2 +1;
+    let  radius = 50 * (4 / (n*3.14));
+    let coef= new coefficient(radius,n);
+
+    coefficients0.push(coef);
+    
+}
+
 let coefficients = [];   //array to fill from the server or the form
 
-function myFunction(){
 
+//from mdn 
+const myForm = document.getElementById('myForm');
+
+myForm.addEventListener('submit', (e) =>{
+
+    e.preventDefault();    //todo rivedere bene questa funzione
+
+    const formData = new FormData(myForm);  //why i don't write this in the arcument
+
+    const data={
+      radius:formData.get('Xn'),
+      angle:formData.get('angle')
+    }
+
+    console.log(data.angle);
+
+    fetch('/form',{
+      method: 'POST',
+      headers:{"Content-type": "application/json"},
+      body : JSON.stringify(data)
+    }).then(res=>{
+      return res.json();
+    }).then(obj =>{
+      time=0;
+      coefficients.push(obj)
+    }).catch((err) => {
+      console.error(err);
+    }) 
+})
+
+function myFunction(){
   //here i have to get the data from the form
   const data = {  //data for debug
     radius:63,
@@ -21,30 +75,29 @@ function myFunction(){
     return res.json();
   })
   .then(obj=>{
-    appo=obj;
-    console.log(appo)})
+    coefficients.push(obj);
+    //coefficients[0]
+    // console.log(coefficients[])
+  })
 }
 
-class coefficient {
-  constructor(_radius, _angle) {
-    this.radius = _radius;
-    this.angle = _angle;
-  }
+function rectangular(){
+  fetch('/rectangular')
+  .then(res =>{
+    console.log('Igot a rectangular request')
+    return res.json();
+  }).then(obj =>{
+    //console.log(obj);
+    time=0;
+    coefficients=obj;      //todo, check this
+    //console.log(coefficients)
+    console.log(coefficients[0]);
+    console.log(coefficients[1]);
+  })
 }
 
-console.log(appo)
 
-//get coefficient automatically for rectangular impulse and store in the array for debug 
-for(let i=0;i<40;i++){
 
-    let n= i*2 +1;
-    let  radius = 50 * (4 / (n*3.14));
-    let coef= new coefficient(radius,n);
-
-    coefficients0.push(coef);
-    
-  console.log(coefficients0[i].radius);
-}
 
 let wave = [];  //point of the function
 let num=10;     //number of coefficient
@@ -64,27 +117,26 @@ function draw(){
   let x=0;
   let y=0;
 
-  if(appo!=undefined)
-    {
-      console.log(appo.radius);
-    }
+  //print for debug todo remove
+  // if(coefficients.length!=0){
+  //     console.log(coefficients[0]);
+  //}
 
-  for(let i=0;i<num;i++){
+  for(let i=0;i<coefficients.length;i++){  //before was num
 
      let prevx=x;
      let prevy=y;
 
-    x += coefficients0[i].radius *  cos(coefficients0[i].angle*time);  //todo aggiustare l'indice di sfasamento
-    y += coefficients0[i].radius *  sin(coefficients0[i].angle*time);
+    x += coefficients[i].radius *  cos(coefficients0[i].angle*time);  
+    y += coefficients[i].radius *  sin(coefficients0[i].angle*time);
 
-    
 
     noFill();
     stroke(120);
 
-    ellipse(prevx,prevy,coefficients0[i].radius*2);
+    ellipse(prevx,prevy,coefficients[i].radius*2);
 
-    if(i==num-1)
+    if(i==coefficients.length-1)  //memorizzo in un array i valori del sin
       wave.unshift(y);
 
    //linea che gira intorno al cerchio
@@ -103,10 +155,14 @@ function draw(){
   ellipse(0,y,5,5)
   noFill();
 
-  for (let i = 0 ; i < wave.length; i++){
+  for (let i = 0 ; i < wave.length; i++){   //disegno di tutta l'onda
     vertex(i,wave[i]); 
   }
   endShape();
-  time+=0.02;
+  time-=0.02;
+
+  // if(wave.length>600){
+  //   wave.pop();
+  // }
 
 }
--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const fs=require('fs');
 
 let port = 3000
 let app = express();
-let coefficients   = [];
+
 let radius = [];
 let angle  = [];
 
 
-let server = app.listen(port);
+// let server = 
+app.listen(port);
 
 app.use(express.static('public'));
 
@@ -35,35 +36,46 @@ app.post('/api',(request,response) =>{
  // console.log(radius[0]);
   radius[0]=request.body.radius;
   angle[0]= request.body.angle;
-  console.log(radius[0]);
-  console.log(angle[0]);
-  console.log(request.body);
+  //console.log(radius[0]);
+  //console.log(angle[0]);
+  //console.log(request.body);
   response.json({
     status: 'success',
     radius: radius[0],
     angle: angle[0] 
   });
 })
-// app.get('/debug',(request,response)=>{
-//   console.log('debug is clicked');
-//  // response.json()
-//   response.json({
-//         status: 'success',
-//         radius:1,
-//         angle:1
-//     });
-//  // response.send(debug is clicked);
-// })
+
+
+
+app.post('/form',(request,response)=>{
+  console.log('I got a form request');
+  console.log(request.body);
+
+ // console.log(radius[0]);
+  radius[0]=request.body.radius;
+  angle[0]= request.body.angle;
+  //console.log(radius[0]);
+  //console.log(angle[0]);
+  //console.log(request.body);
+  response.json({
+    status: 'success',
+    radius: radius[0],
+    angle: angle[0] 
+  });
+})
+
 
 console.log("My server running at port " + port);
 
 class coefficient {
   constructor(_amplitude, _phase) {
-    this.amplitude = _amplitude;
-    this.phase = _phase;
+    this.radius = _amplitude;
+    this.angle = _phase;
   }
 }
 
+let rect_coefficients= []
 for(let i=0;i<40;i++){
 
 
@@ -72,56 +84,11 @@ for(let i=0;i<40;i++){
     let  _amplitude = 50 * (4 / (n*3.14));
 
     let coef = new coefficient(_amplitude,n);
-    coefficients.push(coef);
+    rect_coefficients.push(coef);
 }
 
-//module.exports= coefficients
-
-
-
-// const express = require('express');
-// const Datastore = require('nedb');
-// const port = 8001
-// //const fs = require('fs');
-
-// const app = express();
-// app.listen(port,() => {
-//     console.log('sto in ascolto alla porta '+ port)
-// }); 
-
-// //app.get('public/index.html');
-
-// // app.get('/',function(req, res) {
-// //     res.send('<b>hello world</b>');
-
-// //    //console.log('porcodio');
-// //   });
-
-// app.use(express.static('public'))
-// app.use(express.json({limit : '1mb'})); //this protect my server for flooding of huge quantity of data 
-
-
-// const database = new Datastore('database.db');
-// database.loadDatabase();
-
-// //app.get('/')
-
-// app.post('/api',(request, response) => {
-//     console.log('I got a request')
-//     console.log(request.body);          //i want receive only the body
-
-//     const data = request.body; 
-//     const timestamp = Date.now();
-//     data.timestamp = timestamp;
-
-//     database.insert(data);
-//     console.log(database);
-//     //questo è quello che torna al client
-//     response.json({
-//         status: 'success',
-//         timestamp:timestamp,
-//         latitude:data.lat,
-//         longitude:data.lon
-//     });
-// });
+app.get('/rectangular',(req,res)=>{
+  console.log('I got a rectangular request')
+  res.json(rect_coefficients);
+})
 
