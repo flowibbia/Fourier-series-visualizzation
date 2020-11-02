44353669c07701eeae6a4b9ca6717945d1ae7a42
flowibbia <flowing@mail.com>
2020-11-02 20:10:06 +0100

    migliorato e commentato

--- public/sketch.js
+++ public/sketch.js
@@ -1,9 +1,11 @@
 let time = 0;
-
 let coefficients = []; //array to fill from the server with form
 
+let wave = []; //array of point of the function that will interpolate with vertex
+
+//if the page is reloaded, get coefficients from the server
 if (performance.navigation.type == performance.navigation.TYPE_RELOAD) {
-  
+
   fetch("/reload")
     .then((res) => {
       return res.json();
@@ -17,17 +19,19 @@ if (performance.navigation.type == performance.navigation.TYPE_RELOAD) {
     });
 }
 
-//from mdn
+//get the form with Id
 const myForm = document.getElementById("myForm");
 
 myForm.addEventListener("submit", (e) => {
-  e.preventDefault(); //todo rivedere bene questa funzione
 
-  const formData = new FormData(myForm); //why i don't write this in the argument
+  //Cancels the event to send data
+  e.preventDefault(); 
+
+  const formData = new FormData(myForm);
 
   const data = {
     amplitude: formData.get("amplitude"),
-    frequency: formData.get("frequency"),
+    nth: formData.get("nth"),
   };
 
   //put the data on the server
@@ -58,13 +62,14 @@ function rectangular() {
     })
     .then((obj) => {
       time = 0;
-      coefficients = obj; 
+      coefficients = obj;
     })
     .catch((err) => {
       console.log(err);
     });
 }
 
+//get a triangular request from the server
 function triangular() {
   fetch("/triangular")
     .then((res) => {
@@ -72,69 +77,91 @@ function triangular() {
     })
     .then((obj) => {
       time = 0;
-      coefficients = obj; 
+      coefficients = obj;
     })
     .catch((err) => {
       console.log(err);
     });
 }
 
+//clear the coefficient entered both in the client and in the server
 function reset() {
-  fetch("/reset")
-    .then((res) => {
-      return res.json();
-    })
-    .then((obj) => {
-      //reset time and point of the function
-      time = 0;
-      wave = [];
-      coefficients = obj; 
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  // fetch("/reset")
+  //   .then((res) => {
+  //     return res.json();
+  //   })
+  //   .then((obj) => {
+  //     //reset time and point of the function
+  //     time = 0;
+  //     wave = [];
+  //     coefficients = obj;
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   });
+
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState == 4) {
+      xhr.status == 200 ? console.log(xhr.responseText) : console.error('error');
+      time=0;
+      wave=[];
+      coefficients = JSON.parse(xhr.responseText);
+      console.log();
+    }
+  }
+  xhr.open('GET', '/reset');
+  xhr.send();
+
 }
 
-
-let wave = []; //array of point of the function
-// let num=10;     //number of coefficient
-
+//initial environment, is called when the program starts
 function setup() {
+
   createCanvas(1200, 600);
-  stroke(255);
+  stroke(127, 127, 127);
+
 }
 
+//the function is called in loop and draw the sketch.
+//the framerate is 60 frame per second 
 function draw() {
-  background(0, 10, 50);
+  background(0, 10, 40);
 
   //return when coefficients is empty
   if (coefficients.length == 0) return;
 
+  //draw x axis
   line(200, 300, 1000, 300);
-  translate(200, 300);
+  //translate the cordinates
+  translate(300, 300);
 
+  //center of the 1-th circle
   let x = 0;
   let y = 0;
 
   for (let i = 0; i < coefficients.length; i++) {
-    //before was num
 
+    //save the cordinates of the center of the (i-1)-th circle that will update for the i-th circle
     let prevx = x;
     let prevy = y;
 
-    x += coefficients[i].amplitude * cos(coefficients[i].frequency * time);
-    y += coefficients[i].amplitude * sin(coefficients[i].frequency * time);
+    //calculate the center of the (i+1)-th circle
+    x += coefficients[i].amplitude * cos(coefficients[i].nth * time);
+    y += coefficients[i].amplitude * sin(coefficients[i].nth * time);
 
     noFill();
     stroke(120);
 
     ellipse(prevx, prevy, coefficients[i].amplitude * 2);
 
-    if (i == coefficients.length - 1) wave.unshift(y);
+    //save the point of the latest circle in the beginning of array
+    if (i == coefficients.length - 1) 
+      wave.unshift(y); 
 
-    //linea che gira intorno al cerchio
+    
     stroke(255);
-    fill(255);
+    //  fill(255);
     line(prevx, prevy, x, y);
   }
 
@@ -144,21 +171,18 @@ function draw() {
     translate(200, 0);
     line(x - 200, y, 0, y);
 
-    //-200 perchè ho traslato di 200 a destra
     ellipse(0, y, 5, 5);
-    noFill();
 
-    for (let i = 0; i < wave.length; i++) {
-      //disegno di tutta l'onda
+    for (let i = 0; i < wave.length; i++) 
       vertex(i, wave[i]);
-    }
-
+    
     endShape();
   }
 
-  //
+  //increment time for the next loop
   time -= 0.02;
 
+  //remove point too old
   if (wave.length > 600) {
     wave.pop();
   }
--- server.js
+++ server.js
@@ -1,81 +1,86 @@
-const { response } = require('express');
 const express = require('express');
-// const fs=require('fs');
 
 let port = 3000;
-
 let app = express();
 
-app.listen(port);
+app.listen(port,()=>{
+  console.log("listen on port "+ port)
+});
 
+//serve static files in the root address
 app.use(express.static('public'));
 
-app.use(express.json({limit : '1mb'})); 
+//use json middleware
+app.use(express.json());
 
+ //data structure for coefficient
 class coefficient {
-  constructor(_amplitude, _frequency) {
+  constructor(_amplitude, _nth) {
     this.amplitude = _amplitude;
-    this.frequency = _frequency;
+    this.nth = _nth;
   }
 }
+app.get("/",(req,res)=>{
+  console.log(req);
 
+})
 
-let rect_coefficients= [];
-let triang_coefficients=[];
+//
+let rect_coefficients = [];
+let triang_coefficients = [];
 
-//fill vectors with appropriate coefficient 
-for(let i=0;i<15;i++){
+//fill vectors with appropriate coefficient for rectangular and triangular wave
+for (let i = 0; i < 15; i++) {
+  let n = i * 2 + 1;
+  let amplitude = 100 * (4 / (n * 3.14));
 
-    let n= i*2 +1;
-    let  amplitude = 100 * (4 / (n*3.14));
+  let rect_coef = new coefficient(amplitude, n);
+  rect_coefficients.push(rect_coef);
 
-    let rect_coef = new coefficient(amplitude,n);
-    rect_coefficients.push(rect_coef);
-
-    amplitude = 30*((16/(3.14))*((-1)**((n-1)/2))/(n**2));
-    triang_coef = new coefficient(amplitude,n);
-    triang_coefficients.push(triang_coef);
-    
+  amplitude = 30 * ((16 / (3.14)) * ((-1) ** ((n - 1) / 2)) / (n ** 2));
+  triang_coef = new coefficient(amplitude, n);
+  triang_coefficients.push(triang_coef);
 }
 
+//array for save form data
+let coefficients = [];
 
-let coefficients= []; 
-
-
-app.get("/reset",(req,res)=>{
+app.get("/reset", (req, res) => {
   //delete coefficient and response with empty array
   coefficients = [];
   res.json(coefficients);
 })
 
-app.get("/reload",(req,res)=>{
+app.get("/reload", (req, res) => {
   //response with array saved in server
   res.json(coefficients);
 })
 
-app.post('/form',(request,response)=>{
+//manage form post request
+app.post('/form', (request, response) => {
 
   console.log('I got a form request');
   coefficients.push(request.body);
-  console.log(coefficients[coefficients.length-1]);
+  console.log(coefficients[coefficients.length - 1]);
 
   response.json({
     status: 'success',
-    amplitude: coefficients[coefficients.length-1].amplitude,
-    frequency: coefficients[coefficients.length-1].frequency 
+    amplitude: coefficients[coefficients.length - 1].amplitude,
+    nth : coefficients[coefficients.length - 1].nth
   });
 });
 
-app.get('/rectangular',(req,res)=>{
+//response for rectangular wave request
+app.get('/rectangular', (req, res) => {
   console.log('I got a rectangular request')
-  coefficients=[];
+  coefficients = [];
   res.json(rect_coefficients);
 })
 
-app.get('/triangular',(req,res) =>{
-  console.log('I got a triangular request') 
-  coefficients=[];
+//response for triangular wave request
+app.get('/triangular', (req, res) => {
+  console.log('I got a triangular request')
+  coefficients = [];
   res.json(triang_coefficients);
-})
+}) 
 
-console.log("My server running at port " + port);
